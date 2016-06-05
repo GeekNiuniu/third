a0fb7ba6f692d265e14c6ce8f1c2690aee866860
ajax请求json <niuxiuyue@163.com>
2016-06-05 21:09:28 +0800

    Update calculate-median.js

--- src/calculate-median.js
+++ src/calculate-median.js
@@ -15,8 +15,8 @@ var len=Math.ceil(c.length/2)+1;
 
 	if(len%2==0)
 	return (c[len/2]+c[len/2-1])/2;
-                else
-	return c[Math.floor(len/2-1)];
+               if(len%2==1)
+	return c[Math.floor(len/2)-1];
 
   }
 
